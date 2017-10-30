19a88c4b764c25b91d8e9067c028e785b49dd480
taoteg <taoteg@gmail.com>
2017-10-30 00:09:30 -0500

    Added player entity and controls.

--- Level1.js
+++ Level1.js
@@ -2,18 +2,73 @@ console.log('Level1.js');
 
 Game.Level1 = function(game) {};
 
+var levelConfig = {};
+levelConfig.gravity = 1400;
+
 var map;
+var tile = {};
+tile.width = 64;
+tile.height = 64;
 var layer;
+
+var player;
+var controls = {};
+var playerConfig = {};
+playerConfig.speed = 150;
+playerConfig.jumpTimer = 0;
 
 Game.Level1.prototype = {
   create:function() {
     this.stage.backgroundColor = "AA0099";
-    map = this.add.tilemap('map',64,64);
+    this.physics.arcade.gravity.y = levelConfig.gravity;
+
+    map = this.add.tilemap('map',tile.width,tile.height);
     map.addTilesetImage('tileset');
+    map.setCollisionBetween(0,2);  // 0,0 or 0,2. depends on tileset.
+
     layer = map.createLayer(0);
     layer.resizeWorld();
+
+    player = this.add.sprite(100,500,'player');
+    player.anchor.setTo(0.5,0.5);
+    player.animations.add('idle',[0,1],1,true);
+    player.animations.add('jump',[2],1,true);
+    player.animations.add('run',[3,4,5,6,7,8],7,true);
+    this.physics.arcade.enable(player);
+    this.camera.follow(player);
+    player.body.collideWorldBounds = true;
+
+    controls = {
+      right: this.input.keyboard.addKey(Phaser.Keyboard.D),
+      left: this.input.keyboard.addKey(Phaser.Keyboard.A),
+      up: this.input.keyboard.addKey(Phaser.Keyboard.W),
+    };
+
   },
   update:function() {
+    this.physics.arcade.collide(player,layer);
 
+    player.body.velocity.x = 0;
+
+    if (controls.up.isDown) {
+      player.animations.play('jump');
+    }
+
+    if (controls.right.isDown) {
+      player.animations.play('run');
+      player.scale.setTo(1,1);
+      player.body.velocity.x += playerConfig.speed;
+    }
+
+    if (controls.left.isDown) {
+      player.animations.play('run');
+      player.scale.setTo(-1,1);
+      player.body.velocity.x -= playerConfig.speed;
+    }
+
+    if (controls.up.isDown && (player.body.onFloor() || player.body.touching.down) && this.time.now > playerConfig.jumpTimer) {
+      player.body.velocity.y = -600;
+      playerConfig.jumpTimer = this.time.now + 750;
+    }
   },
 }
--- Preloader.js
+++ Preloader.js
@@ -10,9 +10,15 @@ Game.Preloader.prototype = {
     this.preloaderBar.anchor.setTo(0.5,0.5);
     this.time.advancedTiming = true;
     this.load.setPreloadSprite(this.preloaderBar);
+
     // LOAD ALL ASSETS.
+
+    // Maptiles.
     this.load.tilemap('map','assets/level1.csv');
     this.load.image('tileset','assets/tileset.png');
+
+    // Player sprite.
+    this.load.spritesheet('player','assets/player.png',24,26);
   },
   create:function() {
     this.state.start('Level1');
